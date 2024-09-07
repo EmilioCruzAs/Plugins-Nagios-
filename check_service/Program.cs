40ad27bf8f671fbf97a95c1156359dc9baa84ca4
EmilioCruzAs <emilioalejandrocs6@gmail.com>
2024-09-06 21:07:34 -0600

    Get difference

--- check_service/Program.cs
+++ check_service/Program.cs
@@ -1,36 +1,15 @@
 ﻿using System.Net.Sockets;
-using System.Net;
 using System.Net.Security;
-using System.Security.Authentication;
-using System.Runtime.CompilerServices;
-using Microsoft.VisualBasic;
 using System.Security.Cryptography.X509Certificates;
-using System.Security.Cryptography;
-using System.Timers;
+
 
 class GetCertificateInfo
 {
+    //private static int WARNING = 1;
+    private static int CRITICAL = 2;
+    //private static int OK =0;
     private static readonly RemoteCertificateValidationCallback s_certificateCallback = (_, _, _, _) => true;
-public static void throw_alert (int data)
-{
-    int CRITICAL = 2;
-    int WARNING = 1;
-    int OK = 0;
-    switch (data)
-    {
-        case 1 :
-         Environment.Exit(OK);
-         break;
-        case 2 :
-         Environment.Exit(WARNING);
-         break;
-        case 3:
-        Console.WriteLine("CRITICAL: EL CERTIFICADO  HA CADUCADO");
-        Environment.Exit(CRITICAL);
-        break;
-    }
-    
-}
+
 
 public static async Task<X509Certificate2> GetCertificate(string domain, int port)
 {
@@ -44,7 +23,7 @@ public static async Task<X509Certificate2> GetCertificate(string domain, int por
     var server_certificate = Myssl.RemoteCertificate;
     if(server_certificate != null)
     {
-        return new X509Certificate2(server_certificate);
+         return new X509Certificate2(server_certificate);
     }
     
     return null;
@@ -52,14 +31,23 @@ public static async Task<X509Certificate2> GetCertificate(string domain, int por
 
 public static async Task Main(string[] args)
 {
-    
-        string domain = args[0];
+    if(args.Length==0){
+        Console.WriteLine("CRITICAL: Se requiere un argumento: -domain \n");
+        Console.Write("-help para mas informacion");
+        Environment.Exit(CRITICAL);
+       }else if(args[0]=="-help" || args[0]=="-Help"){
+        Console.WriteLine("Para el funcionamiento correcto del script se requiere el siguiente parametro:\t\t");
+        Environment.Exit(0);
+       }
+    string domain = args[0];
        
-         var get_cert =await GetCertificate(domain, 443);
-         DateTime local = get_cert.NotAfter; 
-         var newdate= ((DateTimeOffset)local).ToUnixTimeSeconds();
-         Console.WriteLine(newdate);
-    
+    var get_cert =await GetCertificate(domain, 443);
+    DateTime Externaldatime = get_cert.NotAfter; 
+    DateTime LocalDatetime = DateTime.Now;
+    TimeSpan difference =Externaldatime-LocalDatetime;
+    var dias = difference.Days;
+    Console.WriteLine(dias);
+
 
 }
 
